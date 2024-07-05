d09348b6881e9450ff9654b358d50e82fba7aeda
Krish <131204733+worhasdev@users.noreply.github.com>
2024-07-05 12:06:31 +0100

    input_class.lua

--- input_class.lua
+++ input_class.lua
@@ -1,8 +1,6 @@
 --[[
-Author(s): Krish "worhas" Purgaus
-Purpose: Handles inputs 
+Author(s): Krish "worhas" Purgaus 
 ]]--
-
 
 local input = {}
 
@@ -10,36 +8,42 @@ local uis = game:GetService("UserInputService")
 local player = game:GetService("Players").LocalPlayer
 
 local buttons = {
-	{ Name = "flashlight", HoldInput = false },
-	{ Name = "crouch", HoldInput = true },
-	{ Name = "interact", HoldInput = false },
-	{ Name = "sprint", HoldInput = false },
-	{ Name = "em1", HoldInput = false},
-	{ Name = "em2", HoldInput = false},
-	{ Name = "em3", HoldInput = false},
+	{ Name = "ability1", HoldInput = false },
+	{ Name = "ability2", HoldInput = false },
+	{ Name = "ability3", HoldInput = false },
+	{ Name = "ability4", HoldInput = false },
+	{ Name = "special", HoldInput = false},
+	{ Name = "sprint", HoldInput = true},
+	{ Name = "block", HoldInput = false},
+	{ Name = "dash", HoldInput = false},
 }
 
 local keyboard_bind = {
-	[Enum.KeyCode.C] = "crouch",
-	[Enum.KeyCode.E] = "interact",
-	[Enum.KeyCode.T] = "em1",
-	[Enum.KeyCode.Y] = "em2",
-	[Enum.KeyCode.U] = "em3",
+	[Enum.KeyCode.One] = "ability1",
+	[Enum.KeyCode.Two] = "ability2",
+	[Enum.KeyCode.Three] = "ability3",
+	[Enum.KeyCode.Four] = "ability4",
+	[Enum.KeyCode.E] = "special",
+	[Enum.KeyCode.LeftShift] = "sprint",
+	[Enum.KeyCode.F] = "block",
+	[Enum.KeyCode.Q] = "dash",
 }
 
 local gamepad_bind = {
-	[Enum.KeyCode.ButtonR2] = "crouch",
-	[Enum.KeyCode.ButtonX] = "interact",
-	[Enum.KeyCode.ButtonY] = "em1",
-	[Enum.KeyCode.ButtonL1] = "em2",
-	[Enum.KeyCode.ButtonR3] = "em3",
+	[Enum.KeyCode.ButtonR2] = "ability1",
+	[Enum.KeyCode.ButtonX] = "ability2",
+	[Enum.KeyCode.ButtonY] = "ability3",
+	[Enum.KeyCode.ButtonL1] = "ability4",
+	[Enum.KeyCode.ButtonR3] = "special",
+	[Enum.KeyCode.ButtonR1] = "sprint",
+	[Enum.KeyCode.ButtonB] = "block",
+	[Enum.KeyCode.ButtonL3] = "dash",
 }
 
 local buttonEvents = {}
+local holdInputState = {}
 
 function input.Init()
-	local holdInputState = {}
-
 	local function isHoldInput(button)
 		return button.HoldInput
 	end
@@ -72,78 +76,40 @@ function input.Init()
 
 	local function onKeyDown(inputObject, isRepeat)
 		if isRepeat then return end
+		local buttonName = nil
 		if inputObject.UserInputType == Enum.UserInputType.Keyboard then
-			local key = inputObject.KeyCode
-			local buttonName = keyboard_bind[key]
-			if buttonName then
-				local button = nil
-				for _, btn in ipairs(buttons) do
-					if btn.Name == buttonName then
-						button = btn
-						break
-					end
-				end
-				if button then
-					if isHoldInput(button) then
-						updateButton(button, true)
-					elseif not getInputState(button) then
-						updateButton(button, true)
-						updateButton(button, false)  -- Set the button state to false straight away
-					end
-				end
-			end
+			buttonName = keyboard_bind[inputObject.KeyCode]
 		elseif inputObject.UserInputType == Enum.UserInputType.Gamepad1 then
-			local key = inputObject.KeyCode
-			local buttonName = gamepad_bind[key]
-			if buttonName then
-				local button = nil
-				for _, btn in ipairs(buttons) do
-					if btn.Name == buttonName then
-						button = btn
-						break
-					end
-				end
-				if button then
-					if isHoldInput(button) then
+			buttonName = gamepad_bind[inputObject.KeyCode]
+		end
+		if buttonName then
+			for _, button in ipairs(buttons) do
+				if button.Name == buttonName then
+					if isHoldInput(button) or not getInputState(button) then
 						updateButton(button, true)
-					elseif not getInputState(button) then
-						updateButton(button, true)
-						updateButton(button, false)  -- Set the button state to false straight away
 					end
+					break
 				end
 			end
 		end
 	end
 
 	local function onKeyUp(inputObject)
+		local buttonName = nil
 		if inputObject.UserInputType == Enum.UserInputType.Keyboard then
-			local key = inputObject.KeyCode
-			local buttonName = keyboard_bind[key]
-			if buttonName then
-				local button = nil
-				for _, btn in ipairs(buttons) do
-					if btn.Name == buttonName then
-						button = btn
-						break
-					end
-				end
-				if button then
-					updateButton(button, false)
-				end
-			end
+			buttonName = keyboard_bind[inputObject.KeyCode]
 		elseif inputObject.UserInputType == Enum.UserInputType.Gamepad1 then
-			local key = inputObject.KeyCode
-			local buttonName = gamepad_bind[key]
-			if buttonName then
-				local button = nil
-				for _, btn in ipairs(buttons) do
-					if btn.Name == buttonName then
-						button = btn
-						break
+			buttonName = gamepad_bind[inputObject.KeyCode]
+		end
+		if buttonName then
+			for _, button in ipairs(buttons) do
+				if button.Name == buttonName then
+					if isHoldInput(button) then
+						updateButton(button, false)
+					else
+						setInputState(button, false)
 					end
-				end
-				if button then
-					updateButton(button, false)
+					break
 				end
 			end
 		end
@@ -166,5 +132,8 @@ function input.GetButtonEvent(self, buttonName)
 	return buttonEvent
 end
 
-return input
+function input.IsInputHeld(buttonName)
+	return holdInputState[buttonName] or false
+end
 
+return input
